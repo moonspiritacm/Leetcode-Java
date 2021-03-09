5e751d6a466b13426fa81bc5c5d71f0710367c2d
moonspiritacm <yefanbupt@gmail.com>
2021-03-09 09:47:23 +0800

    Problem 0125 R2

--- src/com/moonspirit/leetcode/p0125/Solution.java
+++ src/com/moonspirit/leetcode/p0125/Solution.java
@@ -1,7 +1,7 @@
 package com.moonspirit.leetcode.p0125;
 
 /**
- * 回文数——迭代。O(n) O(1)
+ * 判读回文数——迭代。O(n) O(1)
  * <p>
  * 执行用时：3 ms, 在所有 Java 提交中击败了 92.93% 的用户
  * 内存消耗：38.8 MB, 在所有 Java 提交中击败了 19.62% 的用户
@@ -15,7 +15,7 @@ class Solution {
         int l = 0;
         int r = s.length() - 1;
         char[] chs = s.toCharArray();
-        while (l <= r) {
+        while (l < r) {
             if (!Character.isLetterOrDigit(chs[l])) {
                 l++;
             } else if (!Character.isLetterOrDigit(chs[r])) {
--- src/com/moonspirit/leetcode/p0125/Solution2.java
+++ src/com/moonspirit/leetcode/p0125/Solution2.java
@@ -1,18 +1,14 @@
 package com.moonspirit.leetcode.p0125;
 
 /**
- * 回文数——手工判读。O(n) O(1)
+ * 判读回文数——手工判读。O(n) O(1)
  * <p>
  * 执行用时：2 ms, 在所有 Java 提交中击败了 99.87% 的用户
  * 内存消耗：38.3 MB, 在所有 Java 提交中击败了 89.77% 的用户
  */
 class Solution2 {
-    private boolean isVaild(char ch) {
-        if (ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
-            return true;
-        } else {
-            return false;
-        }
+    private boolean isNotVaild(char ch) {
+        return !(ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z');
     }
 
     public boolean isPalindrome(String s) {
@@ -24,9 +20,9 @@ class Solution2 {
         int r = s.length() - 1;
         char[] chs = s.toCharArray();
         while (l <= r) {
-            if (!isVaild(chs[l])) {
+            if (isNotVaild(chs[l])) {
                 l++;
-            } else if (!isVaild(chs[r])) {
+            } else if (isNotVaild(chs[r])) {
                 r--;
             } else {
                 if (chs[l] >= 'a' && chs[l] <= 'z') {

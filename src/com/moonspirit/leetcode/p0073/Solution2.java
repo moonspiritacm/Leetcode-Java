6df493d1578c7fa39bc773d7f65f29e1d7c516c0
moonspiritacm <yefanbupt@gmail.com>
2021-04-14 09:58:03 +0800

    Problem 0073 RQ

--- src/com/moonspirit/leetcode/p0073/Solution2.java
+++ src/com/moonspirit/leetcode/p0073/Solution2.java
@@ -14,7 +14,6 @@ class Solution2 {
         int m = matrix.length;
         int n = matrix[0].length;
         boolean col0 = false;
-        boolean row0 = false;
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 col0 = true;
